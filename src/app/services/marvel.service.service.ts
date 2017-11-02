c8871df2a9aeea2d0f3007de3072ca01e5175222
Daniele <daniele.germani.85@gmail.com>
2017-11-02 16:19:01 +0100

    marvel service work well, and portfolio html and css ok

--- src/app/services/marvel.service.service.ts
+++ src/app/services/marvel.service.service.ts
@@ -15,13 +15,13 @@ export class MarvelService {
     public loadingComics() {
         this.loading = true;
         let promise = new Promise((resolve, reject) => {
-          this.http.get('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=0b42f200217c6f2799d73efdad72146b&hash=c9fdab8e53926323e0b2a0d9e2606054')
+          this.http.get('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=0b42f200217c6f2799d73efdad72146b&hash=c9fdab8e53926323e0b2a0d9e2606054')
             .subscribe(res => {
               this.characters = res.json().data.results
               this.loading = false;
               resolve();
               console.log(this.characters)
-              
+
             });
         });
         return promise;
